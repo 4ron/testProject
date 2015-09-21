b26c3f52dc4a83de214d51b9e7b1284dfd0af551
4ron <aronhjartarson@gmail.com>
2015-09-21 20:16:01 +0200

    added remote db url

--- models/database.js
+++ models/database.js
@@ -5,12 +5,15 @@
 var pg = require('pg');
 
 //var conString = "postgres://konstantinkunz:mamamia@localhost:5432/konstantinkunz";
-var conString = "postgres://postgres:aron@localhost:5432/testdb1";
+//var conString = "postgres://postgres:aron@localhost:5432/testdb1";
+var conString = process.env.DATABASE_URL;
+
 // TODO: add heroku database. Was not possible due to heroku service downtime.
 
 // make sure your local db has a table called task_table
 exports.addTaskToDb = function(task) {
     pg.connect(conString, function(err, client, done) {
+    //pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         console.log(task);
         console.log(typeof task);
         client.query("INSERT INTO task_table(tasktext, prio, deadline, complete) " +
