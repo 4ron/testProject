fce6fe6b8fcd94eeb5d81bb7115dbb0820acf575
4ron <aronhjartarson@gmail.com>
2015-09-22 23:02:27 +0200

    displayed tasks in tasklist

--- models/database.js
+++ models/database.js
@@ -11,7 +11,6 @@ var conString = process.env.DATABASE_URL;
 // make sure your local db has a table called task_table
 exports.addTaskToDb = function(task) {
     pg.connect(conString, function(err, client, done) {
-        console.log(client);
         client.query("INSERT INTO task_table(tasktext, prio, deadline, complete) " +
             "VALUES($1, $2, $3, $4)", [task.tasktext, task.prio, task.deadline, false], function(err, result) {
             done();
@@ -20,3 +19,20 @@ exports.addTaskToDb = function(task) {
         });
     });
 };
+
+exports.queryAllTasks = function(res, callback) {
+    pg.connect(conString, function(err, client, done) {
+        console.log(client);
+        client.query("SELECT * FROM task_table", function(err, result) {
+            done();
+            if (err) {
+                console.error(err); response.send("Error " + err);
+            } else {
+                console.log(result);
+                // somehow render the tasklist with the results
+                callback(res, result.rows);
+            }
+        });
+    });
+};
+
--- routes/tasklist.js
+++ routes/tasklist.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../models/database');
 
 /* GET list of tasks */
 router.get('/', function(req, res, next) {
-    res.render('tasklist');
+    //res.render('tasklist');
+    database.queryAllTasks(res, doRender);
 });
+
+var doRender = function (res, rows) {
+    res.render('tasklist', {tasks: rows});
+};
 
 module.exports = router;
